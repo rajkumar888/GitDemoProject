9fe9067cf1e0793186a1df9f08ee8142891a4f87
rajkumar888 <Soma@rajeev67>
2022-01-30 12:59:41 +0800

    comments added...

--- src/gitdemo/HelloWorldDemo.java
+++ src/gitdemo/HelloWorldDemo.java
@@ -6,12 +6,15 @@ public class HelloWorldDemo {
 
 	public static void main(String[] args) {
 
+		// demo purpose file creation 
+		
 		
 		System.out.println("program is started at "+new Date());
 		
 		System.out.println("Hello World......");
 		
 		System.out.println("program is ended at "+new Date());
+	
 	}
 
 }
