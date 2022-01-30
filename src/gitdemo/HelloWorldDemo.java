f08b016dfa14e88563de8658bc9a43567ade0394
rajkumar888 <Soma@rajeev67>
2022-01-30 12:52:41 +0800

    first commit for demo purpose

---
+++ src/gitdemo/HelloWorldDemo.java
@@ -0,0 +1,16 @@
+package gitdemo;
+
+import java.util.Date;
+
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Today date is "+new Date());
+		
+		System.out.println("Hello Wordl......");
+		
+	}
+
+}
